a03981ddc05d0249ee2b644c5c85cd3cdcbe9c4d
Marco Prado <macpratony@gmail.com>
2020-04-22 21:00:22 +0200

    Mensaje por pantalla de prueba

--- ProyectoCompartido/src/principal/Main.java
+++ ProyectoCompartido/src/principal/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Codigo escrito Jose 13:41 21 abril");
-
+		System.out.println("Esto es una prueba para git");
 	}
 
 }
